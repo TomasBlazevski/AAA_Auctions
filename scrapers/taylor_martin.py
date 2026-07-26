5593fe591dfe470b6e206c6072e17c617a7eb711
Tomas <tomas.b@aaalease.net>
2026-07-26 19:31:13 +0200

    Modified scrapers with option to delete or not future records

--- scrapers/taylor_martin.py
+++ scrapers/taylor_martin.py
@@ -251,7 +251,7 @@ def main():
     output_file.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(output_file, index=False)
     print(f"Data saved to CSV: {OUTPUT_PATH}")
-    save_upcoming_auctions(df.to_dict(orient="records"), table="tm_trucks_specs")
+    save_upcoming_auctions(df.to_dict(orient="records"), table="tm_trucks_specs", delete_future=True)
 
 
 if __name__ == "__main__":
